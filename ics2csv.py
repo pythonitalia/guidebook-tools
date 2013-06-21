e36153cbd1ea84b16ad37dfe09f8e5f53dbb213d
Giovanni Bajo <rasky@develer.com>
2013-06-21 23:46:15 +0200

    First version

---
+++ ics2csv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import csv
+from icalendar import Calendar, Event
+
+def convert(fn):
+	out = csv.writer(sys.stdout)
+	cal = Calendar.from_ical(open(fn,'rb').read())
+	for event in cal.walk():
+		if isinstance(event, Event):
+			start = event.decoded("DTSTART")
+			end = event.decoded("DTEND")
+
+			out.writerow([
+				event.decoded("SUMMARY"),
+				start.date(),
+				start.time(),
+				end.time(),
+				"",
+				event.decoded("LOCATION"),
+				event.decoded("DESCRIPTION"),
+			])
+
+
+if __name__ == "__main__":
+	convert(sys.argv[1])
